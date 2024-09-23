f94395eea579a399e09556c9f2da7cc24b1b34b3
Kiefer Chang <kiefer.chang@suse.com>
2024-09-23 13:42:47 +0800

    Bump OS to leap-15.5
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- scripts/common_prepare.sh
+++ scripts/common_prepare.sh
@@ -7,5 +7,6 @@ if [ "$provision_net_install" = "false" ]; then
   exit 0
 fi
 
+zypper rr -a && zypper ar  http://free.nchc.org.tw/opensuse/update/leap/15.5/oss/ update && zypper ar http://free.nchc.org.tw/opensuse/distribution/leap/15.5/repo/oss/ oss
 zypper ref
-zypper in -y apparmor-parser iptables
+zypper in -y apparmor-parser iptables k9s kubernetes-client
